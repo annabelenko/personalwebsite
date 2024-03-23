5dc70f6bd3eda39becf48dde293b499f775fc271
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-03-22 20:42:02 -0400

    deleted items

--- script.js
+++ script.js
@@ -52,10 +52,10 @@ function filterSelection(category) {
 
 // Initial call to show all items
 document.addEventListener('DOMContentLoaded', function() {
-    filterSelection('all');
+    filterSelection('software');
 });
 
-
+/*
 
 document.addEventListener('DOMContentLoaded', function() {
     const mover = document.getElementById('mover');
@@ -94,63 +94,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setInterval(moveMover, 20);
 });
-//< -- Stick Figure -->
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const jsonUrl = 'spritesheet.json'; // Adjust this to the path of your JSON file
-
-    fetch(jsonUrl)
-        .then(response => response.json())
-        .then(data => startStickFigureAnimation(data))
-        .catch(error => console.error('Error loading JSON:', error));
-});
-
-//< -- Stick Figure -->
-
-function startStickFigureAnimation(spriteData) {
-    const stickFigure = document.getElementById('stick-figure');
-    let currentFrameIndex = 0;
-    let posX = 0;
-    // Increase the base speed for faster movement
-    let baseSpeed = 60; // Adjusted base speed to make the stick figure move faster
-    let hoverSpeed = baseSpeed / 2; // Hover speed is half of the new base speed
-    let speed = baseSpeed; // Current speed starts at the new base speed
-    let intervalId;
-
-    function updateFrame() {
-        const frame = spriteData.frames[currentFrameIndex].frame;
-        stickFigure.style.backgroundPosition = `-${frame.x}px -${frame.y}px`;
-        currentFrameIndex = (currentFrameIndex + 1) % spriteData.frames.length;
-    }
-
-    function moveStickFigure() {
-        posX += speed;
-        if (posX > window.innerWidth) {
-            posX = -stickFigure.offsetWidth; // Reset position to loop animation
-        }
-        stickFigure.style.left = `${posX}px`;
-    }
-
-    function restartAnimation() {
-        clearInterval(intervalId);
-        intervalId = setInterval(function() {
-            updateFrame();
-            moveStickFigure();
-        }, 50); // Consider lowering this for a smoother animation, but watch out for performance
-    }
-
-    // Start the animation initially
-    restartAnimation();
-
-    stickFigure.addEventListener('mouseover', () => {
-        speed = hoverSpeed; // Decrease speed on hover
-    });
-
-    stickFigure.addEventListener('mouseout', () => {
-        speed = baseSpeed; // Restore speed on mouse out
-    });
-}
 
 
 
@@ -158,3 +101,4 @@ function startStickFigureAnimation(spriteData) {
 
 
 
+*/
